a4c708c06d7a9a7aca71a3b828fdbdb94d657fa2
iqbal <iqbalhonnur@gmail.com>
2019-06-14 19:54:18 +0530

    Second commit Changes : Created Login UI Date: 13-06-2019

---
+++ app/src/main/java/app/com/sekreto/User/UserRegistration.java
@@ -0,0 +1,15 @@
+package app.com.sekreto.User;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.com.sekreto.R;
+
+public class UserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_registration_activity);
+    }
+}
