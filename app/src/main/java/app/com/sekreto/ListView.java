8d6c04368ab71fbf56b097af04f1a229c154c98e
ShyamKunda <shyamk.iiit@gmail.com>
2019-06-19 11:14:51 +0530

    Added toolbar for List view

--- app/src/main/java/app/com/sekreto/ListView.java
+++ app/src/main/java/app/com/sekreto/ListView.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -20,6 +21,9 @@ public class ListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
 
         ArrayList<Item> ItemList = new ArrayList<>();
