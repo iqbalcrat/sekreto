5ec92bdc35ee4256e46c89f5d389318d2dd06a7d
ShyamKunda <shyamk.iiit@gmail.com>
2019-06-29 20:20:36 +0530

    Adding question to firestore DB

---
+++ app/src/main/java/app/com/sekreto/Models/sample.java
@@ -0,0 +1,15 @@
+package app.com.sekreto.Models;
+
+
+import java.time.LocalDate;
+
+public class sample {
+
+
+    public static void main(String[] args){
+
+
+
+        System.out.println( LocalDate.now());
+    }
+}
--- app/src/main/java/app/com/sekreto/QuestionScreen.java
+++ app/src/main/java/app/com/sekreto/QuestionScreen.java
@@ -4,33 +4,88 @@ import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class QuestionScreen extends AppCompatActivity {
+    private static final String TAG = "QuestionScreen";
     Dialog myDialog;
+    private EditText questionText;
+    private Button button;
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
         myDialog = new Dialog(this);
+
     }
+
     public void ShowPopup(View v) {
         TextView txtclose;
         myDialog.setContentView(R.layout.questionpopup);
         txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
         txtclose.setText("X");
 
+        questionText = myDialog.findViewById(R.id.questionText);
+        button = myDialog.findViewById(R.id.storeInDBButton);
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myDialog.dismiss();
             }
         });
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                Log.d(TAG , questionText.getText().toString());
+
+                Map<String, Object> question = new HashMap<>();
+                question.put("question", questionText.getText().toString());
+
+
+                db.collection("Questions")
+                        .add(question)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+
+
+
+
+            }
+        });
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         myDialog.show();
     }
+
+
 }
